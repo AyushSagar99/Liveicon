d03b7199c19071f6e7974a5b4201103124d79b5d
Ayush Sagar <saggar.ayush@gmail.com>
2026-01-09 15:18:46 +0530

    modal

--- components/icon.tsx
+++ components/icon.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { useState } from "react";
 import { ICONS } from "@/utils/icon";
 import { ICON_REGISTRY } from "@/utils/iconRegistry";
+import { Modal } from "./modal";
 
 export default function IconGrid() {
+  const [open, setOpen] = useState(false);
+  const [selectedIcon, setSelectedIcon] = useState<keyof typeof ICON_REGISTRY | null>(null);
+
   return (
     <div className="select-none mx-auto max-w-[1200px] p-4">
       <div className="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-7 lg:grid-cols-9 xl:grid-cols-10 gap-4">
@@ -12,8 +17,11 @@ export default function IconGrid() {
 
           return (
             <div
-            onClick={()=>{console.log("hello")}}
               key={id}
+              onClick={() => {
+                setSelectedIcon(name);
+                setOpen(true);
+              }}
               className="backdrop-blur-xl bg-white/20 border border-white/30 
               rounded-xl p-6 shadow-lg flex items-center justify-center cursor-pointer"
             >
@@ -22,6 +30,15 @@ export default function IconGrid() {
           );
         })}
       </div>
+
+      {open && selectedIcon && (
+        <Modal onClose={() => setOpen(false)}>
+          {(() => {
+            const Icon = ICON_REGISTRY[selectedIcon];
+            return <Icon size={64} />;
+          })()}
+        </Modal>
+      )}
     </div>
   );
 }
--- components/modal.tsx
+++ components/modal.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client";
 
-function Modal() {
+import React from "react";
+
+export function Modal({
+  onClose,
+  children,
+}: {
+  onClose: () => void;
+  children: React.ReactNode;
+}) {
   return (
-    <div>Modal</div>
-  )
-}
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      
+      <div
+        className="absolute inset-0 bg-black/30 backdrop-blur-sm"
+        onClick={onClose}
+      />
 
-export default Modal
+      <div
+        className="relative z-10 w-[50%] h-[50%] p-2 bg-fuchsia-200 rounded-2xl shadow-2xl flex items-start justify-start"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="flex justify-center items-center p-4 border rounded-2xl drop-shadow-xl bg-fuchsia-300">
+        {children}
+        </div>
+      </div>
+
+    </div>
+  );
+}
